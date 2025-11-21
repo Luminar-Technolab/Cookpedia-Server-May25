8a026070de9c68ff51bd2997d80788cab4bbcbbc
soniagkalliath <soniageorge111@gmail.com>
2025-11-21 11:05:39 +0530

    register added

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+const users = require('../models/userModel')
+const bcrypt = require('bcrypt')
+
+//register
+exports.registerController = async (req,res)=>{
+    console.log("Inside registerController");
+    const {username,email,password} = req.body
+    try{
+        const existingUser = await users.findOne({email})
+        if(existingUser){
+            res.status(406).json("User Already exists... Please Login!!!")
+        }else{
+            const encryptPassword =  await bcrypt.hash(password,10)
+            const newUser = new users({
+                username,email,password:encryptPassword,profile:""
+            })
+            await newUser.save()
+            res.status(200).json(newUser)
+        }
+    }catch(err){
+        res.status(500).json(err)
+    }
+}
--- routing/routes.js
+++ routing/routes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const recipeController = require('../controllers/recipeController')
+const userController = require('../controllers/userController')
 
 const routes = express.Router()
 
 //get all recipes
 routes.get('/recipes/all',recipeController.getAllRecipesController)
-
+//register
+routes.post('/register',userController.registerController)
 
 module.exports = routes
